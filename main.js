e1bd5bb40a7269e9dbb478de3e18c8961f973b3b
Jagatperumal <74633014+Jagatperumal@users.noreply.github.com>
2021-03-10 18:35:24 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,67 @@
+function getParagraph1(){
+var input= [];
+
+}
+for( i= 1 ; i=<6 ; i++){
+inputs.push(document.getElementById("input_1" +i).value);
+
+}
+inputs.join(". ");
+document.getElementById("showParagraph1").innerHTML=inputs.join(" .");
+
+function getParagraph2(){
+    var input= [];
+    
+    }
+    for( i= 2 ; i=<6 ; i++){
+    inputs.push(document.getElementById("input_2" +i).value);
+    
+    }
+    inputs.join(". ");
+    document.getElementById("showParagraph2").innerHTML=inputs.join(" .");
+
+    function getParagraph3(){
+        var input= [];
+        
+        }
+        for( i= 3 ; i=<6 ; i++){
+        inputs.push(document.getElementById("input_3" +i).value);
+        
+        }
+        inputs.join(". ");
+        document.getElementById("showParagraph3").innerHTML=inputs.join(" .");
+
+        function getParagraph4(){
+            var input= [];
+            
+            }
+            for( i= 4 ; i=<6 ; i++){
+            inputs.push(document.getElementById("input_4" +i).value);
+            
+            }
+            inputs.join(". ");
+            document.getElementById("showParagraph4").innerHTML=inputs.join(" .");
+
+            function getParagraph5(){
+                var input= [];
+                
+                }
+                for( i= 5 ; i=<6 ; i++){
+                inputs.push(document.getElementById("input_5" +i).value);
+                
+                }
+                inputs.join(". ");
+                document.getElementById("showParagraph5").innerHTML=inputs.join(" .");
+                function getParagraph6(){
+                    var input= [];
+                    
+                    }
+                    for( i= 6 ; i=<6 ; i++){
+                    inputs.push(document.getElementById("input_6" +i).value);
+                    
+                    }
+                    inputs.join(". ");
+                    document.getElementById("showParagraph6").innerHTML=inputs.join(" .");
+    
+
+      
